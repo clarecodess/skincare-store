f7eaaa9fa8eaf92b43a65f5d4b7b95e8c6ada096
Clare Codes <clarecodess@gmail.com>
2024-05-29 14:03:39 +0300

    get currently signed user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Checkout from "./components/Checkout";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  useEffect(() =>{
+    //run once when app component loads
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+          console.log("uid", uid)
+        } else {
+          console.log("user is logged out")
+        }
+      } )
+  },[])
+
   return (
     <BrowserRouter>
       <div className="app">
